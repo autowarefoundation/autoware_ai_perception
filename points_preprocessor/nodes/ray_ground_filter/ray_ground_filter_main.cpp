50a0045b3ae4d8af4a0e69e25abb451f8663d171
YamatoAndo <yamato.ando@gmail.com>
2019-10-31 13:54:09 +0000

    fixed to ground filter in base_link
    Signed-off-by: Yamato ANDO <yamato.ando@gmail.com>

--- points_preprocessor/nodes/ray_ground_filter/ray_ground_filter_main.cpp
+++ points_preprocessor/nodes/ray_ground_filter/ray_ground_filter_main.cpp
@@ -1,15 +1,30 @@
+/*
+ * Copyright 2017-2019 Autoware Foundation. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ********************
+ * amc-nu (abrahammonrroy@yahoo.com)
+ */
 
 #include <ros/ros.h>
-#include "ray_ground_filter.h"
+#include "points_preprocessor/ray_ground_filter/ray_ground_filter.h"
 
-
-int main(int argc, char **argv)
+int main(int argc, char** argv)
 {
-	ros::init(argc, argv, "ray_ground_filter");
-	RayGroundFilter app;
+  ros::init(argc, argv, "ray_ground_filter");
+  RayGroundFilter app;
 
-	app.Run();
+  app.Run();
 
-	return 0;
-
+  return 0;
 }
