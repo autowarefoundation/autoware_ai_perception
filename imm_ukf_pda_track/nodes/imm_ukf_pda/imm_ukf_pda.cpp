790f7141fea4fa77199623c2a67ecae43b4b5bf4
icolwell-as <48106342+icolwell-as@users.noreply.github.com>
2020-11-06 12:12:35 -0500

    Cleanup logging spam from lidar_imm_ukf_pda_track (#32)
    * Merge branch '18_imm_ukf_console_spam' into 'as/master'
    
    Cleanup logging spam from lidar_imm_ukf_pda_track
    
    Closes #18
    
    See merge request astuff/autoware.ai/core_perception!45
    
    Signed-off-by: Ian Colwell <icolwell@autonomoustuff.com>
    
    * Use simpler topic names, remap to appropriate namespaces in launch
    
    Signed-off-by: Ian Colwell <icolwell@autonomoustuff.com>

--- imm_ukf_pda_track/nodes/imm_ukf_pda/imm_ukf_pda.cpp
+++ imm_ukf_pda_track/nodes/imm_ukf_pda/imm_ukf_pda.cpp
@@ -25,7 +25,7 @@ ImmUkfPda::ImmUkfPda()
   has_subscribed_vectormap_(false),
   private_nh_("~")
 {
-  private_nh_.param<std::string>("tracking_frame", tracking_frame_, "world");
+  private_nh_.param<std::string>("tracking_frame", tracking_frame_, "map");
   private_nh_.param<int>("life_time_threshold", life_time_threshold_, 8);
   private_nh_.param<double>("gating_threshold", gating_threshold_, 9.22);
   private_nh_.param<double>("gate_probability", gate_probability_, 0.99);
@@ -54,8 +54,8 @@ ImmUkfPda::ImmUkfPda()
 
 void ImmUkfPda::run()
 {
-  pub_object_array_ = node_handle_.advertise<autoware_msgs::DetectedObjectArray>("/detection/objects", 1);
-  sub_detected_array_ = node_handle_.subscribe("/detection/fusion_tools/objects", 1, &ImmUkfPda::callback, this);
+  pub_object_array_ = node_handle_.advertise<autoware_msgs::DetectedObjectArray>("objects_out", 1);
+  sub_detected_array_ = node_handle_.subscribe("objects_in", 1, &ImmUkfPda::callback, this);
 
   if (use_vectormap_)
   {
